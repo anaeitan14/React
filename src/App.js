4df5e1217fdd8671d415202db687aeb96b80b930
emoky <eitanlich@yahoo.com>
2022-12-20 15:35:22 +0200

    20/12

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import Header from "./Components/Header";
-import Contact from "./Components/Contact";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import FormAdd from "./components/FormAdd"
 import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   state = {
@@ -9,19 +11,29 @@ class App extends Component {
       {name:"Doron", email:"doron.ex@gmail.com", phone:"054-1231232"},
       {name:"Eitan", email:"eitanlich2000@gmail.com", phone:"555-444-222"},
       {name:"Yossi", email:"shlomo@shlomo.net", phone:"111-122-232"},
-      {name:"Eran LEAVE :rage:", email:"shlomo@shlomo.net", phone:"111-122-232"},
     ]
   } 
 
   handleDelete = (id) => {
-    alert(id);
+    const areYouSure = window.confirm(`Do you want to delete the contact ${this.state.users[id].name}?`)
+    if(areYouSure) {
+      let tempUsers = this.state.users;
+      tempUsers.splice(id,1);
+      this.setState({users : tempUsers})
+    }
+  }
+
+
+  handleAdd = () => {
+    alert('add')
   }
 
   render() { 
     return (
       <div>
         <Header brand='Contact-List'/>
-        <div>
+        <div className='container p-2'>
+        <FormAdd onAdd={this.handleAdd}/>
           {this.state.users.map((person, idx) => (
           <Contact 
             key={idx} 
